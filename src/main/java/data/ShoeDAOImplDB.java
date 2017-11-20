ef14b3f85f10dd0286e7fc200763a072358a5073
Alex Perez <perezidential@Alexs-MacBook-Pro.local>
2017-11-19 17:30:19 -0700

    added new impl

---
+++ src/main/java/data/ShoeDAOImplDB.java
@@ -0,0 +1,49 @@
+package data;
+
+import java.util.List;
+
+public class ShoeDAOImplDB implements ShoeDAO {
+
+	@Override
+	public List<Shoe> getAllShoes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addShoe(Shoe s) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Shoe> getShoeByBrand(String brand) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Shoe getShoeByIndex(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Shoe> getShoeByRpriceLessThanEqualTo(int rprice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Shoe updateShoe(Shoe s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteShoe(Shoe s) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
